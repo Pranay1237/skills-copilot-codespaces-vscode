2641f7c51cc57127ff7788f1be7fdcfd16c8faf5
Pranay <106003280+Pranay1237@users.noreply.github.com>
2023-08-20 13:07:39 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,35 +1,74 @@
 // create web server
 const express = require('express');
-const app = express();
 const bodyParser = require('body-parser');
+const cors = require('cors');
+const { randomBytes } = require('crypto')
 const axios = require('axios');
 
-// use body-parser middleware
+const app = express();
 app.use(bodyParser.json());
+app.use(cors());
 
-// handle post request
+const commentsByPostId = {};
+
+// get comments by post id
+app.get('/posts/:id/comments', (req, res) => {
+  res.send(commentsByPostId[req.params.id] || []);
+});
+
+// create comment by post id
+app.post('/posts/:id/comments', async (req, res) => {
+  const commentId = randomBytes(4).toString('hex');
+  const { content } = req.body;
+
+  // get comments by post id
+  const comments = commentsByPostId[req.params.id] || [];
+
+  // push new comment to comments array
+  comments.push({ id: commentId, content, status: 'pending' });
+
+  // set comments by post id
+  commentsByPostId[req.params.id] = comments;
+
+  // emit event to event bus
+  await axios.post('http://event-bus-srv:4005/events', {
+    type: 'CommentCreated',
+    data: { id: commentId, content, postId: req.params.id, status: 'pending' }
+  });
+
+  res.status(201).send(comments);
+});
+
+// handle event from event bus
 app.post('/events', async (req, res) => {
+  console.log('Event Received:', req.body.type);
+
   const { type, data } = req.body;
 
-  // handle comment moderation
-  if (type === 'CommentCreated') {
-    const status = data.content.includes('orange') ? 'rejected' : 'approved';
+  // handle event
+  if (type === 'CommentModerated') {
+    const { id, postId, status, content } = data;
 
-    // emit event
+    // get comments by post id
+    const comments = commentsByPostId[postId];
+
+    // find comment by id
+    const comment = comments.find(comment => comment.id === id);
+
+    // set status
+    comment.status = status;
+
+    // emit event to event bus
     await axios.post('http://event-bus-srv:4005/events', {
-      type: 'CommentModerated',
-      data: {
-        id: data.id,
-        postId: data.postId,
-        status,
-        content: data.content,
-      },
+      type: 'CommentUpdated',
+      data: { id, postId, status, content }
     });
   }
+
   res.send({});
 });
 
-// listen to port 4003
-app.listen(4003, () => {
-  console.log('Listening on 4003');
+// listen port
+app.listen(4001, () => {
+  console.log('Listening on 4001');
 });
